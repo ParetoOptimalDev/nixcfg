ab0715386a7c928bbb43909e9fa45cbc3dc4febb
Christian Harke <christian@harke.ch>
2021-11-28 17:06:08 +0100

    Refactor home-manager input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, home-manager, nixpkgs }:
