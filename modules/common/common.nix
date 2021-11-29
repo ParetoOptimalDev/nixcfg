d78824c07f4082d0743311c98af41efb54d3af26
Christian Harke <christian@harke.ch>
2021-11-29 21:38:21 +0100

    Disable impure auto upgrade

--- modules/common/common.nix
+++ modules/common/common.nix
@@ -107,16 +107,12 @@ in
       };
     };
 
-  system = {
-    autoUpgrade = {
-      enable = true;
-      allowReboot = false;
-      channel = https://nixos.org/channels/nixos-20.09;
+  system =
+    {
+      # This value determines the NixOS release with which your system is to be
+      # compatible, in order to avoid breaking some software such as database
+      # servers. You should change this only after NixOS release notes say you
+      # should.
+      stateVersion = import ../../version.nix;
     };
-    # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-    # servers. You should change this only after NixOS release notes say you
-    # should.
-    stateVersion = import ../../version.nix;
-  };
 }
