fea7b606941a9ebdaaab640074747112e830477a
Christian Harke <christian@harke.ch>
2021-11-29 10:03:26 +0100

    Make shell aliases available in all shells

--- home/common/default.nix
+++ home/common/default.nix
@@ -91,6 +91,46 @@
       web ++
       dev ++
       office;
+    shellAliases = {
+      # List the last ten directories we've been to this session, no duplicates
+      d = "dirs -v | head -10";
+      cat = "bat";
+      grep = "rg";
+      l = "exa -hl --git --icons";
+      la = "exa -ahl --git --icons";
+      ll = "l";
+      ls = "ls -sh --color='auto'";
+      lsa = "ls -a";
+      tree = "l --tree";
+
+      # File manager
+      rr = "ranger";
+
+      # Tmuxinator
+      mux = "tmuxinator";
+
+      # Calendar shortcuts
+      cal = "khal -v ERROR calendar 2>/dev/null";
+      yesterday = "cal yesterday 24h --format '{start-end-time-style} {title}'";
+      today = "cal today 24h --format '{start-end-time-style} {title}'";
+      tomorrow = "cal tomorrow 24h --format '{start-end-time-style} {title}'";
+
+      # Open Fontawesome icon selector
+      fa = "fontawesome-menu -f icon-list.txt";
+
+      # Java REPL
+      jshell = "nix-shell -p openjdk --command jshell";
+      visualvm = "visualvm --cp:a ~/jmx/jmxremote_optional.jar";
+
+      # Git CLI UI
+      lg = "lazygit";
+
+      # PDF viewer
+      mupdf = "mupdf-x11";
+
+      # Password manager
+      pass = "source pass";
+    };
     sessionVariables = {
       EDITOR = "vim";
     };
