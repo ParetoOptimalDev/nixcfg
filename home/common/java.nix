06222785cd9f87cff0831d21e0c07cd3b5a4cafc
Christian Harke <christian.harke@bluecare.ch>
2021-11-30 17:58:52 +0100

    Remove conflicting java derivations

--- home/common/java.nix
+++ home/common/java.nix
@@ -3,9 +3,7 @@
 let
 
   java = pkgs.jdk;
-  java8 = pkgs.jdk8;
-  java11 = pkgs.jdk11;
-  jfx11 = pkgs.openjfx11;
+  jfx= pkgs.openjfx11;
 
 in
 
@@ -13,21 +11,15 @@ in
   home = {
     packages = [
       java
-      java8
-      java11
-      jfx11
+      jfx
     ];
 
     sessionVariables = {
       JAVA_HOME = "${java}/lib/openjdk";
-      JAVA8_HOME = "${java8}/lib/openjdk";
-      JAVA11_HOME = "${java11}/lib/openjdk";
       JDK_HOME = "${java}/lib/openjdk";
-      JDK8_HOME = "${java8}/lib/openjdk";
-      JDK11_HOME = "${java11}/lib/openjdk";
 
       # IntelliJ IDEA Code with me
-      INTELLIJCLIENT_JDK = "${java11}/lib/openjdk";
+      INTELLIJCLIENT_JDK = "${java}/lib/openjdk";
     };
   };
 }
