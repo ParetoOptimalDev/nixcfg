af5959be4c3193d6dc389a031b9080dc2ffb31fb
Christian Harke <christian@harke.ch>
2021-12-02 18:04:14 +0100

    Upgrade to release 21.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
