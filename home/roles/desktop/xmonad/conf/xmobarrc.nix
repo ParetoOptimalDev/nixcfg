4df2af05e5f8cc4c83e0bd3a8959d79cbf957897
Christian Harke <christian.harke@bluecare.ch>
2022-01-20 00:39:07 +0100

    Add battery monitor to xmobar

--- home/roles/desktop/xmonad/conf/xmobarrc.nix
+++ home/roles/desktop/xmonad/conf/xmobarrc.nix
@@ -1,6 +1,7 @@
 ''
   Config { overrideRedirect = False
          , font     = "xft:VictorMono Nerd Font:style=SemiBold:pixelsize=14:antialias=true"
+         , additionalFonts  = ["xft:FontAwesome-9"]
          , alpha    = 128
          , bgColor  = "#5f5f5f"
          , fgColor  = "#f8f8f2"
@@ -25,6 +26,16 @@
                           , "--"
                           , "--on", ""
                           ]
+                      , Run Battery
+                          [ "-t", "<acstatus><watts> (<left>%)"
+                          , "-L", "10", "-H", "80", "-p", "3"
+                          , "--", "-O", "<fc=green>On</fc> - ", "-i", ""
+                          , "-L", "-15", "-H", "-5"
+                          , "-l", "red", "-m", "blue", "-h", "green"
+                          , "--lows"   , "<fn=1>\62020</fn>  "
+                          , "--mediums", "<fn=1>\62018</fn>  "
+                          , "--highs"  , "<fn=1>\62016</fn>  "
+                          ] 10
                       , Run Memory ["--template", "Mem: <usedratio>%"] 10
                       , Run Swap [] 10
                       , Run Date "%a %Y-%m-%d <fc=#8be9fd>%H:%M</fc>" "date" 10
@@ -32,6 +43,6 @@
                       ]
          , sepChar  = "%"
          , alignSep = "}{"
-         , template = "%StdinReader% }{ %alsa:default:Master% | %cpu% | %memory% * %swap% | %EGPF% | %date% "
+         , template = "%StdinReader% }{ %alsa:default:Master% | %cpu% | %memory% * %swap% | %battery% | %EGPF% | %date% "
          }
 ''
