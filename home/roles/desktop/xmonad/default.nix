331c716336abcc58cd72464d3ff4703a9f81115b
Christian Harke <christian.harke@bluecare.ch>
2022-01-19 13:52:41 +0100

    Enable picom compositor on xmonad

--- home/roles/desktop/xmonad/default.nix
+++ home/roles/desktop/xmonad/default.nix
@@ -28,18 +28,21 @@ with pkgs;
     extraConfig = import ./conf/xmobarrc.nix;
   };
 
-  services.trayer = {
-    enable = true;
-    settings = {
-      edge = "top";
-      align = "right";
-      SetDockType = true;
-      SetPartialStrut = true;
-      expand = true;
-      width = 10;
-      transparent = true;
-      tint = "0x5f5f5f";
-      height = 22;
+  services = {
+    picom = import ../picom;
+    trayer = {
+      enable = true;
+      settings = {
+        edge = "top";
+        align = "right";
+        SetDockType = true;
+        SetPartialStrut = true;
+        expand = true;
+        width = 10;
+        transparent = true;
+        tint = "0x5f5f5f";
+        height = 22;
+      };
     };
   };
 
