826a1ef7391a97b1c51ab63a08260acfc16dd7a7
Christian Harke <christian.harke@bluecare.ch>
2022-01-19 22:20:43 +0100

    Update dunst config

--- home/roles/desktop/dunst/default.nix
+++ home/roles/desktop/dunst/default.nix
@@ -8,66 +8,66 @@
 
   services.dunst = {
     enable = true;
+    iconTheme = {
+      package = pkgs.paper-icon-theme;
+      name = "Paper";
+      size = "16x16";
+    };
     settings = {
       global = {
-        font = "VictorMono Nerd Font Regular 11";
-        markup = "yes";
-        plan_text = "no";
-        format = "<b>%s %p</b>\n%b";
-        sort = "no";
-        indicate_hidden = "yes";
-        alignment = "left";
-        bounce_freq = 0;
-        show_age_threshold = -1;
-        word_wrap = 0;
-        ignore_newline = "no";
-        stack_duplicates = "yes";
-        hide_duplicates_count = "no";
-        geometry = "1000x50-10+35";
-        shrink = "yes";
-        transparency = 5;
-        idle_threshold = 2;
         monitor = 0;
-        follow = "keyboard";
-        sticky_history = "yes";
-        history_length = 15;
-        show_indicators = "no";
-        line_height = 3;
+        follow = "none";
+        width = "(0,1000)";
+        height = "50";
+        notification_limit = "6";
+        origin = "top-right";
+        offset = "15x36";
+        indicate_hidden = "true";
+        transparency = 5;
         separator_height = 2;
         padding = 6;
         horizontal_padding = 12;
-        separator_color = "frame";
-        startup_notification = false;
-        dmenu = "${pkgs.dmenu}/dmenu -p dunst:";
-        browser = "${pkgs.firefox}/firefox -new-tab";
-        icon_position = "left";
-        max_icon_size = 80;
-        icon_path = "/usr/share/icons/Paper/16x16/mimetypes/:/usr/share/icons/Paper/48x48/status/:/usr/share/icons/Paper/16x16/devices/:/usr/share/icons/Paper/48x48/notifications/:/usr/share/icons/Paper/48x48/emblems/";
         frame_width = 2;
-        frame_color = "#8EC07C";
-      };
-      shortcuts = {
-        close = "ctrl+space";
-        close_all = "ctrl+shift+space";
-        context = "ctrl+shift+period";
+        separator_color = "auto";
+        sort = "true";
+        idle_threshold = 2;
+        font = "VictorMono Nerd Font Regular 11";
+        line_height = 3;
+        format = "<b>[%a] %s %p</b>\\n%b";
+        show_age_threshold = -1;
+        ignore_newline = "false";
+        stack_duplicates = "true";
+        hide_duplicate_count = "false";
+        show_indicators = "no";
+        icon_position = "left";
+        max_icon_size = 40;
+        sticky_history = "true";
+        history_length = 20;
+        dmenu = "${pkgs.dmenu}/bin/dmenu";
+        browser = "${pkgs.xdg-utils}/bin/xdg-open";
+
+        # RULES
+        word_wrap = "false";
+        alignment = "left";
+        markup = "full";
       };
       urgency_low = {
         frame_color = "#3B7C87";
         foreground = "#3B7C87";
         background = "#191311";
-        timeout = 4;
+        timeout = "4s";
       };
       urgency_normal = {
         frame_color = "#5B8234";
         foreground = "#5B8234";
         background = "#191311";
-        timeout = 6;
+        timeout = "6s";
       };
       urgency_critical = {
         frame_color = "#B7472A";
         foreground = "#B7472A";
         background = "#191311";
-        timeout = 8;
+        timeout = "8s";
       };
     };
   };
