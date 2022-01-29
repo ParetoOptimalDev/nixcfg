26ba432b321c1dc760adba51e278c09d8160915e
Christian Harke <christian.harke@bluecare.ch>
2022-01-29 15:37:23 +0100

    Reenable n75 specific modules

--- hosts/n75/default.nix
+++ hosts/n75/default.nix
@@ -1,21 +1,22 @@
-{ pkgs, rootPath, ... }:
+{ pkgs, rootPath, ... } @ args:
 
 let
 
   username = "christian";
+  moduleArgs = args // { inherit username; };
 
 in
 
 {
   imports = [
     ./hardware
-    #(import ./devmail { inherit pkgs rootPath; })
-    #(import ./fileSystems { inherit pkgs username; })
-    #(import ./openvpn { inherit username; })
-    #./printing
+    ./devmail
+    (import ./fileSystems moduleArgs)
+    (import ./openvpn moduleArgs)
+    ./printing
   ];
 
-  custom.base.users.usernames = [ "christian" ];
+  custom.base.users.usernames = [ username ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
