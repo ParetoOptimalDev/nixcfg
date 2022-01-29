543ffbcb2fc403c38a5c96fb9127ee1f225e5c22
Christian Harke <christian.harke@bluecare.ch>
2022-01-29 16:41:32 +0100

    Inline state version again

--- nixos/base/default.nix
+++ nixos/base/default.nix
@@ -112,7 +112,7 @@ in
         # compatible, in order to avoid breaking some software such as database
         # servers. You should change this only after NixOS release notes say you
         # should.
-        stateVersion = import (rootPath + "/version.nix");
+        stateVersion = "21.11";
       };
 
       time.timeZone = "Europe/Zurich";
