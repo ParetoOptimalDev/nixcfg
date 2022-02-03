11d0eb68e126b938a255b3785f025946edff2a58
Christian Harke <christian.harke@bluecare.ch>
2022-02-03 15:01:18 +0100

    Refactor wallpapers config

--- home/roles/desktop/feh/default.nix
+++ home/roles/desktop/feh/default.nix
@@ -12,6 +12,12 @@ in
   options = {
     custom.roles.desktop.feh = {
       enable = mkEnableOption "Feh image and background util";
+
+      wallpapersDir = mkOption {
+        type = types.path;
+        default = config.home.homeDirectory + "/Pictures/wallpapers";
+        description = "Directory containing wallpapers";
+      };
     };
   };
 
@@ -19,7 +25,7 @@ in
     programs.feh.enable = true;
 
     xsession.initExtra = ''
-      ${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ~/Pictures/wallpapers
+      ${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ${cfg.wallpapersDir}
     '';
   };
 }
--- home/roles/desktop/grobi/default.nix
+++ home/roles/desktop/grobi/default.nix
@@ -18,21 +18,28 @@ in
         default = [ ];
         description = "Grobi rules";
       };
+
+      wallpapersDir = mkOption {
+        type = types.path;
+        default = config.home.homeDirectory + "/Pictures/wallpapers";
+        description = "Directory containing wallpapers";
+      };
     };
   };
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      feh
       grobi
       xorg.xrandr
     ];
+
+    programs.feh.enable = true;
 
     services.grobi = {
       inherit (cfg) rules;
       enable = true;
       executeAfter = [
-        "${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize $HOME/Pictures/wallpapers"
+        "${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize ${cfg.wallpapersDir}"
       ];
     };
   };
