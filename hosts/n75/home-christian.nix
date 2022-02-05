6788efbf6e1bb6399045f165dd54557d0daf5549
Christian Harke <christian.harke@bluecare.ch>
2022-02-05 14:42:51 +0100

    Move grobi config to separate file

--- hosts/n75/home-christian.nix
+++ hosts/n75/home-christian.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./home/grobi
     ./home/kmonad
     ./home/xbindkeys
   ];
@@ -14,35 +15,6 @@
     roles = {
       desktop = {
         enable = true;
-        grobi = {
-          enable = true;
-          rules = [
-            {
-              name = "Single";
-              outputs_connected = [ "DP-2" ];
-              outputs_disconnected = [ "DP-0" "DP-1" "HDMI-0" ];
-              configure_single = "DP-2";
-              primary = "DP-2";
-              atomic = true;
-              execute_after = [
-                "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 1920x1080 --rate 60"
-                "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --panning 1920x1080+0+0"
-              ];
-            }
-            {
-              name = "Home-Office";
-              outputs_connected = [ "DP-2" "HDMI-0-BNQ-32554-21573-ZOWIE XL LCD-5AG00657SL0" ];
-              outputs_disconnected = [ "DP-0" "DP-1" ];
-              configure_row = [ "HDMI-0" "DP-2" ];
-              primary = "HDMI-0";
-              atomic = true;
-            }
-            {
-              name = "Fallback";
-              configure_single = "DP-2";
-            }
-          ];
-        };
         mobile.enable = true;
       };
       dev.enable = true;
---
+++ hosts/n75/home/grobi/default.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  custom.roles.desktop.grobi = {
+    enable = true;
+    rules = [
+      {
+        name = "Single";
+        outputs_connected = [ "DP-2" ];
+        outputs_disconnected = [ "DP-0" "DP-1" "HDMI-0" ];
+        configure_single = "DP-2";
+        primary = "DP-2";
+        atomic = true;
+        execute_after = [
+          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 1920x1080 --rate 60"
+          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --panning 1920x1080+0+0"
+        ];
+      }
+      {
+        name = "Home-Office";
+        outputs_connected = [ "DP-2" "HDMI-0-BNQ-32554-21573-ZOWIE XL LCD-5AG00657SL0" ];
+        outputs_disconnected = [ "DP-0" "DP-1" ];
+        configure_row = [ "HDMI-0" "DP-2" ];
+        primary = "HDMI-0";
+        atomic = true;
+      }
+      {
+        name = "Fallback";
+        configure_single = "DP-2";
+      }
+    ];
+  };
+}
