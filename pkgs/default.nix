712098ed6d48b3f10fcf00563ed1115600efd334
Christian Harke <christian.harke@bluecare.ch>
2022-02-12 00:15:50 +0100

    Flakify hinclient

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,8 +2,6 @@ pkgs:
 let
   callPackage = pkgs.lib.callPackageWith (custom // pkgs);
   custom = pkgs // {
-    hinclient = callPackage ./hinclient { };
-
     # Overriding
     # spectrwm = callPackage ./spectrwm {};
   };
--- pkgs/modules.nix
+++
@@ -1,3 +0,0 @@
-[
-  ./hinclient/module.nix
-]
