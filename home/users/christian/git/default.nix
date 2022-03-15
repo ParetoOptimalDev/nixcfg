19ff291e8fc67b532b2a7c437adb11653dfd6cb1
Christian Harke <christian.harke@bluecare.ch>
2022-03-15 21:13:37 +0100

    Enable autosquash on interactive git rebase shortcut

--- home/users/christian/git/default.nix
+++ home/users/christian/git/default.nix
@@ -35,7 +35,7 @@ in
         ignored = "ls-files --exclude-standard --ignored --others";
         lg = "log --graph --full-history --all --color --pretty=format:'%x1b[33m%h%x09%C(blue)(%ar)%C(reset)%x09%x1b[32m%d%x1b[0m%x20%s%x20%C(dim white)-%x20%G?%x20%an%C(reset)'";
         rc = "rebase --continue";
-        ri = "rebase --interactive";
+        ri = "rebase --interactive --autosquash";
         rs = "rebase --skip";
         s = "status";
         ss = "status -s";
