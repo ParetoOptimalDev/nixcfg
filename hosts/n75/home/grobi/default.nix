6024fd5936479f1b7695f02f85d05d1661122ea4
Christian Harke <christian.harke@bluecare.ch>
2022-05-10 14:14:18 +0200

    Add grobi rules for office displays

--- hosts/n75/home/grobi/default.nix
+++ hosts/n75/home/grobi/default.nix
@@ -24,6 +24,28 @@
         atomic = true;
       }
       {
+        name = "Office-0";
+        outputs_connected = [ "DP-2" "DP-0.8" "DP-0.1" ];
+        outputs_disconnected = [ "DP-0" "DP-1" "HDMI-0" ];
+        configure_row = [ "DP-2" "DP-0.8" "DP-0.1" ];
+        primary = "DP-0.8";
+        atomic = true;
+        execute_after = [
+          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --auto --output DP-0.8 --auto --primary --above DP-2 --output DP-0.1 --auto --right-of DP-0.8"
+        ];
+      }
+      {
+        name = "Office-1";
+        outputs_connected = [ "DP-2" "DP-1.8" "DP-1.1" ];
+        outputs_disconnected = [ "DP-0" "DP-1" "HDMI-0" ];
+        configure_row = [ "DP-2" "DP-1.8" "DP-1.1" ];
+        primary = "DP-1.8";
+        atomic = true;
+        execute_after = [
+          "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --auto --output DP-1.8 --auto --primary --above DP-2 --output DP-1.1 --auto --right-of DP-1.8"
+        ];
+      }
+      {
         name = "Fallback";
         configure_single = "DP-2";
       }
