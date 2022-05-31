426a6d895ec3aa77866f0b3573bba46650d9ec68
Christian Harke <christian.harke@bluecare.ch>
2022-05-31 14:18:43 +0200

    Fix grobi home-office rules

--- hosts/n75/home/grobi/default.nix
+++ hosts/n75/home/grobi/default.nix
@@ -6,7 +6,7 @@
       {
         name = "Single";
         outputs_connected = [ "DP-2" ];
-        outputs_disconnected = [ "DP-0" "DP-1" "HDMI-0" ];
+        outputs_disconnected = [ "DP-0" "DP-0.1" "DP-0.8" "DP-1" "DP-1.1" "DP-1.8" "HDMI-0" ];
         configure_single = "DP-2";
         primary = "DP-2";
         atomic = true;
@@ -18,7 +18,7 @@
       {
         name = "Home-Office";
         outputs_connected = [ "DP-2" "HDMI-0-BNQ-32554-21573-ZOWIE XL LCD-5AG00657SL0" ];
-        outputs_disconnected = [ "DP-0" "DP-1" ];
+        outputs_disconnected = [ "DP-0" "DP-0.1" "DP-0.8" "DP-1" "DP-1.1" "DP-1.8" ];
         configure_row = [ "HDMI-0" "DP-2" ];
         primary = "HDMI-0";
         atomic = true;
